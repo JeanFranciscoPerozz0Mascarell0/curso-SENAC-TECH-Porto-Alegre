5d4e1be6e0c9f1c93b12d1f669d6aff9792afc49
JeanFranciscoPerozz0Mascarell0 <148917757+JeanFranciscoPerozz0Mascarell0@users.noreply.github.com>
2023-11-09 23:04:59 -0300

    Update CALCULO IMC.py

--- CALCULO IMC.py
+++ CALCULO IMC.py
@@ -1,25 +1,34 @@
 pessoas = []
 
-#def imc(peso = 0, altura = 0):
-#    resultado = peso / (altura*altura)
-#    return pessoas.append(resultado)
+def calcular_imc(peso = 0, altura = 0):
+    resultado = peso / (altura**2)
 
-#def imprimir():
-#    print(f"O usuario {pessoas[0]['nome']} está com {pessoas[0]['nome']} ")
+    if resultado >= 17 and resultado <= 18.4:
+        return f'está com o IMC de {resultado:.2f}, e está com magreza leve.'
+    elif resultado <= 16.9 and resultado >= 16:
+        return f'está com o IMC de {resultado:.2f}, e está com magreza moderada.'
+    elif resultado < 16:
+        return f'está com o IMC de {resultado:.2f}, e está com magreza grave.'
+    elif resultado <= 29.9 and resultado >= 25:
+        return f'está com o IMC de {resultado:.2f}, e está com sobrepeso.'
+    elif resultado <= 34.9 and resultado >= 30:
+        return f'está com o IMC de {resultado:.2f}, e está com obesidade grau 1.'
+    elif resultado <= 39.9 and resultado >= 35:
+        return f'está com o IMC de {resultado:.2f}, e está com obesidade severa.'
+    elif resultado >= 40:
+        return f'está com o IMC de {resultado:.2f}, e está com obesidade mórbida.'
+    else:
+        return f'está com o IMC de {resultado:.2f}, e está no peso ideal.'
 
+def imprimir(imprima = False):
+    for c in range(len(pessoas)):
+        print(f"{pessoas[c]['nome']} {calcular_imc(pessoas[c]['peso'], pessoas[c]['altura'])}")
 
 for pessoa in range(0,5):
     nome = input("Digite seu nome: ")
     altura = float(input("Insira sua altura: "))
     peso = float(input("Insira seu peso: "))
     print('-'*50)
-
-    variavel_imc = peso / (altura * altura)
-    
     pessoas.append({'nome': nome, 'altura' : altura, 'peso' : peso})
 
-    #imc(peso, altura)
-
-
- 
-
+imprimir(True)
