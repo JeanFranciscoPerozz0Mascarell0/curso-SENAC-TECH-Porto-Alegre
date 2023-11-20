a0e949ea7f53a2bc75020643a99a5f84f2e734ae
JeanFranciscoPerozz0Mascarell0 <148917757+JeanFranciscoPerozz0Mascarell0@users.noreply.github.com>
2023-11-20 17:24:39 -0300

    Add files via upload
    ATUALIZEI NO SENAC E NÃO ENTENDI MAIS NADA

--- OOP livraria/Livraria.py
+++ OOP livraria/Livraria.py
@@ -1,32 +1,47 @@
-from datetime import date 
+import datetime as dt
 class Livraria:
-    def __init__(self, nome, cpf, endereco, telefone):
-        self.nome = nome
-        self.cpf = cpf
-        self.endereco = endereco
-        self.telefone = telefone
+    livros = [
+    ["O Sol é para Todos","Harper Lee", "Romance, Justiça Social", "Editora José Olympio", "9788503009490", 10], 
+    ["Sapiens: Uma Breve História da Humanidade", "Yuval Noah Harari", "História, Antropologia", "L&PM Editores", "9788525432187", 10], 
+    [ "A Menina que Roubava Livros", "Markus Zusak", "Ficção Histórica", "Intrínseca", "9788580573494", 10], 
+    ["O Poder do Hábito", "Charles Duhigg", "Psicologia, Desenvolvimento Pessoal", "Objetiva", "9788539004111", 10], 
+    ["Norwegian Wood", "Haruki Murakami", "Ficção Contemporânea", "Editora Objetiva", "9788539004111", 10]]
 
-        
-        
-    def cadastro(self):
-        self.nome = input("Insira o nome do usuario: ")
-        self.telefone = input("Insira o telefone: ")
-        self.endereco = input("Insira o endereço: ")
-        self.cpf = input("Insira o cpf: ")
+    clientes = [
+        {'nome' : 'Jean', 'endereco':'Rua Alberto Torres - 135', 'telefone' : '(54) 99688-1202', 'cpf' : '02973374081'}
+    ]
+    
+    def __init__(self):
+        print("\nBem vindo a Livraria!\nPara realizar o login digite:")
+        self.nome = input("Insira seu nome: ")
+        self.cpf = input("Insira seu cpf: ")
 
-        self.cliente = [self.nome, self.telefone, self.endereco, self.cpf]
+    def login(self, clientes):
+        livros_comprado = []
+        data_do_livro = []
+        for pessoa in range(0, len(clientes)):
+            if self.nome in clientes[pessoa]['nome'] and self.cpf in clientes[pessoa]['cpf']:
+                print(f"Bem Vindo {clientes[pessoa]['nome']}!")
+                print(f"Seu telefone é: {clientes[pessoa]['telefone']}.")
+                print(f"Seu endereço é: {clientes[pessoa]['endereco']}.")
+                print(f"Seu cpf é: {clientes[pessoa]['cpf']}.")
 
+                print(f"Deseja comprar algum livro? ")
+                escolha = input(f"[S] [N]: ")
+
+                while escolha == 's':
+                    livros_comprado.append(input("Digite o nome do livro que quer comprar: "))
+                    data_do_livro.append(str(dt.date.today()))
+                    
+                compras = {'livros':livros_comprado,
+                            'data do livro': data_do_livro}
+                     
+                
+            else:
+                print(f"Ocorreu um erro! Usuario não encontrado!")
+    
+        return compras
+    
+    #def cadastro(self):
         
     
-    def inserindo_livros(self):
-        self.livros = {}
-        escolha = 'S'
-        while escolha == 'S':        
-            livro_escolhido = input("Insira o nome do livro escolhido: ")
-            dia_de_hoje = str(date.today())
-            self.livros[livro_escolhido] = dia_de_hoje
-            escolha = input("Deseja comprar algum livro? ").upper() 
-        
-        self.cliente.append(self.livros)
-        
-
--- OOP livraria/livraria_executavel.py
+++ OOP livraria/livraria_executavel.py
@@ -1,4 +1,13 @@
-class Livraria:
-    def __init__(self):
+from Livraria import Livraria as l
 
-    
+def main():
+    livros = l.livros
+    clientes = l.clientes
+
+    pessoa = l()
+    livro_nome = l.login(pessoa,clientes)
+    clientes[0].update({'nome do livro' : livro_nome})
+    print(clientes)
+
+if __name__ == '__main__':
+    main()
